7b89e875b7bc48ae52cbce699efa56a675886d7c
zhongzc <zhongzc_arch@outlook.com>
2020-01-09 22:59:22 +0800

    add readme

--- src/main/scala/com/gaufoo/Config.scala
+++ src/main/scala/com/gaufoo/Config.scala
@@ -10,7 +10,7 @@ object Config {
   val G: Int = (1 << 10) * M
 
   val BLOCK_SIZE: Int          = 8 * K
-  val CACHED_BLOCK_COUNT: Int  = (20 * M) / BLOCK_SIZE
+  val CACHED_BLOCK_COUNT: Int  = (1 * G) / BLOCK_SIZE
   val BLOCK_KV_COUNT_HINT: Int = 512
 
   val BLK_UNKNOWN: Byte  = 0
